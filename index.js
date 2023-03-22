80e21a685806d25fe2c183837e2e9b50b37c3866
Samlehoy <muttaqien0111@gmail.com>
2023-03-22 17:17:21 +0700

    V1.02
    Update category hasil BMI sudah bisa

--- index.js
+++ index.js
@@ -12,22 +12,28 @@ function resetBMI(event){
 	event.preventDefault();
 	const result = document.getElementById("result");
 }
+	if(form){
 	form.addEventListener('submit', handleForm);
 	form.addEventListener('reset', resetBMI);
+	}
 
+//Fungsi kalkulasi
 function calculateBMI() {
 	// Get input values
-	let weight = parseFloat(document.getElementById("weight").value);
-	let height = parseFloat(document.getElementById("height").value);
-	let gender = document.getElementById("gender").value;
-	let categoryArea = document.querySelector(".comment");
+	var weight = parseFloat(document.getElementById("weight").value);
+	var height = parseFloat(document.getElementById("height").value);
+	var gender = document.getElementById("gender").value;
+	let resultArea = document.querySelector(".comment");
 	const result = document.getElementById("result");
+
 	// Check apakah input sudah benar
 	if (isNaN(weight) || isNaN(height) || gender === "") {
 		alert("Please enter valid input values.");
+		modal.style.display = "block";
+		modalText.innerHTML = `All fields are required!`;
 		return false;
 	}
-	
+
 	// Convert tinggi ke meter
 	height /= 100;
 	
@@ -38,30 +44,38 @@ function calculateBMI() {
 	bmi = bmi.toFixed(2);
 	
 	// Menentukan kategori BMI berdasarkan gender
-	let category;
-	if (gender === "male") {
+	let category = '';
+	if(gender === "male"){
 	
-		if (bmi < 20.7) {
-			category = "Underweight";
-		} else if (bmi < 26.4) {
-			category = "Normal";
-		}
-	} else 	if (gender === "female") {
+		if(bmi<18.5){
+		 category = 'Underweight';
+		  }else if (bmi < 24.9){
+		 category = 'Underweight';
+		  }else if (bmi < 29.9){
+		 category = 'Underweight';
+		  }else if (bmi > 30.0){
+		 category = 'Underweight';
+		  }
+		
+	}else if(gender === "female"){
 	
-		if (bmi < 20.7) {
-			category = "Underweight";
-		} else if (bmi < 26.4) {
-			category = "Normal";
-		}
-		// Klasifikasi berat badan (not working)
-		categoryArea.style.display = "block";
-		document.querySelector(".comment").innerHTML = `You are <span id="comment">${category}</span>`;
-		document.querySelector("#category").innerHTML = bmi.toFixed(2);
-			
-	} else{
+		if(bmi<18.5){
+		 category = 'Underweight';
+		  }else if (bmi < 24.9){
+		 category = 'Underweight';
+		  }else if (bmi < 29.9){
+		 category = 'Underweight';
+		  }else if (bmi > 30.0){
+		 category = 'Underweight';
+		  }
+
 	}
 	// Alert untuk menampilkan hasil BMI
-	alert(category)
 	result.innerHTML = bmi;
+	resultArea.style.display = "block";
+	document.querySelector(".comment").innerHTML = `You are <span id="comment">${category}</span>`;
+	document.querySelector("#result").innerHTML = bmi;
 	return false
+
+
 }
